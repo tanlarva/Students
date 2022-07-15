0fe2f4a28098eb822bc34609292a9f773c76c20b
tanlarva <phanduytan475@gmail.com>
2022-07-15 12:00:45 +0700

    1

--- force-app/main/default/lwc/lWC_TimKiem/lWC_TimKiem.js
+++ force-app/main/default/lwc/lWC_TimKiem/lWC_TimKiem.js
@@ -1,8 +1,28 @@
 import { LightningElement, wire, api, track } from 'lwc';
-import getListClass from '@salesforce/apex/AR_Student_Controller.getListClass';
+import getListClass from '@salesforce/apex/AR_TimKiem_Controller.getListClass';
+import getAllStudent from '@salesforce/apex/AR_TimKiem_Controller.getAllStudent';
+
+const actions = [
+    { label: 'Cập nhật', name: 'update' },
+    { label: 'Xóa', name: 'delete' }
+];
+
+const columns = [
+    { label: 'Họ', fieldName: 'firstName__c', type: 'text' },
+    { label: 'Tên', fieldName: 'lastName__c', type: 'url' },
+    { label: 'Giới Tính', fieldName: 'Sex__c', type: 'text'},
+    { label: 'Ngày Sinh', fieldName: 'dayOfBirth__c'},
+    { label: 'Điểm Hóa', fieldName: 'Diem1__c', type: 'text'},
+    { label: 'Điểm Toán', fieldName: 'Diem2__c', type: 'text'},
+    { label: 'Điểm Lý', fieldName: 'Diem3__c', type: 'text'},
+    { label: 'Điểm Trung Bình', fieldName: 'GPA__c', type: 'text'},
+    { label: 'Tình Trạng', fieldName: 'Status__c', type: 'text'},
+    { type: 'action', typeAttributes: { rowActions: actions } }
+];
 
 export default class LWC_TimKiem extends LightningElement {
     @track listClass;
+    @track dataStudent;
     @wire(getListClass)
     wiredClass({error, data}) {
         if (data) {
@@ -13,9 +33,31 @@ export default class LWC_TimKiem extends LightningElement {
             this.contacts = undefined;
         }
     };
-    get listClass() {
-        return this.listClass;
-    }
+    @wire(getAllStudent)
+    wiredStudent({error, data}) {
+        if (data) {
+            console.log('data:', JSON.stringify(data));
+            console.log('typeof:', typeof data);
+            this.dataStudent = Object.values(data);
+            console.log('this.dataStudent:', JSON.stringify(this.dataStudent));
+            console.log('typeof:', typeof this.dataStudent);
+            this.error = undefined;
+        } else if (error) {
+            console.log('error:', error);
+            this.error = error;
+            this.contacts = undefined;
+        }
+    };
+
+    
+
+    // get dataStudent() {
+    //     return this.dataStudent;
+    // }
+
+    // get listClass() {
+    //     return this.listClass;
+    // }
     handleChangeClass(event) {
         this.chooseClass= event.detail.value;
     }
